596808a40c73a3b1359da785bdd023f758b7fe58
hasbilol <135036552+hasbilol@users.noreply.github.com>
2023-06-10 01:46:02 +0800

    Update StrawBoatArrows.java

--- StrawBoatArrows.java
+++ StrawBoatArrows.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package borrowingarrows;
 
-/**
- *
- * @author User
- */
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
